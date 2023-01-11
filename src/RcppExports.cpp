2a3bab4d57834dc014741cd81a7d4ecd07685359
Gregory Jefferis <jefferis@gmail.com>
2023-01-11 22:27:22 +0000

    rerun rcpp exports

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // dracodecode
 List dracodecode(RawVector data, const int index_offset);
 RcppExport SEXP _dracor_dracodecode(SEXP dataSEXP, SEXP index_offsetSEXP) {
